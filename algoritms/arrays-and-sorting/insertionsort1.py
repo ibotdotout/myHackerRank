6a468d2506b4e0c645ce7f9995fd7e06242d8521
ibotdotout <tkroputa@gmail.com>
2014-06-15 18:19:44 +0700

    update insertionsort1 to use yield

--- algoritms/arrays-and-sorting/insertionsort1.py
+++ algoritms/arrays-and-sorting/insertionsort1.py
@@ -3,19 +3,16 @@
 
 def insert_sort(l):
     display_list = l[:]  # copy l to display_list without ref
-    result = []
     for i in range(len(l) - 1, 0, -1):
         if l[i-1] > l[i]:
             display_list[i] = display_list[i-1]
             l[i], l[i-1] = l[i-1], l[i]
-            result.append(display_list)
+            yield display_list
             display_list = l[:]
-    result.append(l)
-    return result
+    yield l
 
 if __name__ == "__main__":
     N = input()
     l = map(int, raw_input().split())
-    result = insert_sort(l)
-    for i in result:
+    for i in insert_sort(l):
         print ' '.join(map(str, i))
--- algoritms/arrays-and-sorting/insertionsort1_test.py
+++ algoritms/arrays-and-sorting/insertionsort1_test.py
@@ -5,30 +5,30 @@ import insertionsort1 as ins
 
 
 class InsertionSortTest(unittest.TestCase):
+    def setUp(self):
+        self.l = [2, 4, 6, 8, 3]
+        self.results = [l for l in ins.insert_sort(self.l)]
+
     def test_round1_get_24688(self):
-        l = [2, 4, 6, 8, 3]
         n = 0
         expected_result = [2, 4, 6, 8, 8]
-        result = ins.insert_sort(l)
-        self.assertEqual(expected_result, result[n])
+        result = self.results[n]
+        self.assertEqual(expected_result, result)
 
     def test_round2_get_24668(self):
-        l = [2, 4, 6, 8, 3]
         n = 1
         expected_result = [2, 4, 6, 6, 8]
-        result = ins.insert_sort(l)
-        self.assertEqual(expected_result, result[n])
+        result = self.results[n]
+        self.assertEqual(expected_result, result)
 
     def test_round3_get_24468(self):
-        l = [2, 4, 6, 8, 3]
         n = 2
         expected_result = [2, 4, 4, 6, 8]
-        result = ins.insert_sort(l)
-        self.assertEqual(expected_result, result[n])
+        result = self.results[n]
+        self.assertEqual(expected_result, result)
 
     def test_round4_get_23468(self):
-        l = [2, 4, 6, 8, 3]
         n = 3
         expected_result = [2, 3, 4, 6, 8]
-        result = ins.insert_sort(l)
-        self.assertEqual(expected_result, result[n])
+        result = self.results[n]
+        self.assertEqual(expected_result, result)
