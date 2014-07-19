997c86cb52f6137c67954e9f525d6df9e63b8ef1
ibotdotout <tkroputa@gmail.com>
2014-07-19 11:05:03 +0700

    add run all tests file

---
+++ run_tests.sh
@@ -0,0 +1,7 @@
+nosetests miscellaneous/python-tutorials
+nosetests contests/w6
+nosetests contests/projecteuler
+nosetests algoritms/warmup
+nosetests algoritms/search
+nosetests algoritms/math
+nosetests algoritms/arrays-and-sorting
