4d8c4fe4d89b6e002f9e327db4716d8e9062d29f
ibotdotout <tkroputa@gmail.com>
2014-08-12 15:37:16 +0700

    remove unused code

--- algoritms/dynamic-programming/red_john_is_back.py
+++ algoritms/dynamic-programming/red_john_is_back.py
@@ -12,23 +12,21 @@ def solve(n):
 
 
 def generate_primes():
-    """ 
+    """
         Gereate primes with Sieve algorithm
         http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
     """
-    global sieve
     zeros = [0] * (len(sieve) / 2)
     for i in xrange(2, int(len(sieve) ** 0.5) + 1):
         if sieve[i] != 0:
             sieve[i*i::i] = zeros[:((len(sieve)-1)/i) - i + 1]
-    # Count how many primes is less equal than i 
+    # Count how many primes is less equal than i
     for i in xrange(2, len(sieve)):
         sieve[i] += sieve[i-1]
 
 
 def gernerate_arrage_brick():
     """ f(n) = f(n-1) + f(n-4) where f(0)=f(1)=f(2)=f(3)=1 """
-    global brick
     for i in xrange(4, 41):
         brick.append(brick[-1] + brick[-4])
 
