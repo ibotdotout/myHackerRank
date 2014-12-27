f19ca2ac785cc8d1a4a14dce4b588cbce434d42e
ibotdotout <tkroputa@gmail.com>
2014-12-27 20:50:42 +0700

    add tests runner

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,11 @@
 nosetests miscellaneous/python-tutorials
+nosetests miscellaneous/regex
 nosetests contests/w6
 nosetests contests/projecteuler
 nosetests algoritms/warmup
 nosetests algoritms/search
 nosetests algoritms/math
 nosetests algoritms/arrays-and-sorting
+nosetests algoritms/combinatorics
+nosetests algoritms/dynamic-programming
+nosetests algoritms/graph
