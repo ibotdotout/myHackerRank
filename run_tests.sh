1225de96061334857c98ccd46a914d43cbac5d6a
ibotdotout <tkroputa@gmail.com>
2015-01-04 11:02:59 +0700

    update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,14 @@
-nosetests miscellaneous/python-tutorials
-nosetests miscellaneous/regex
-nosetests contests/w6
-nosetests contests/projecteuler
-nosetests algoritms/warmup
-nosetests algoritms/search
-nosetests algoritms/math
-nosetests algoritms/arrays-and-sorting
-nosetests algoritms/combinatorics
-nosetests algoritms/dynamic-programming
-nosetests algoritms/graph
+#!/bin/bash
+
+cmd="nosetests"
+$cmd miscellaneous/python-tutorials
+$cmd miscellaneous/regex
+$cmd contests/w6
+$cmd contests/projecteuler
+$cmd algoritms/warmup
+$cmd algoritms/search
+$cmd algoritms/math
+$cmd algoritms/arrays-and-sorting
+$cmd algoritms/combinatorics
+$cmd algoritms/dynamic-programming
+$cmd algoritms/graph
