80b5f09504fe4d3338bff1bb6107223b9981f3f0
ibotdotout <tkroputa@gmail.com>
2015-01-14 08:10:13 +0700

    add warmup/solve me second

--- algoritms/warmup/solve_me_second.py
+++ algoritms/warmup/solve_me_second.py
@@ -3,3 +3,10 @@
 
 def solve(a, b):
     return a+b
+
+
+if __name__ == '__main__':
+    t = input()
+    for _ in range(t):
+        a, b = tuple(int(i) for i in raw_input().split())
+        print solve(a,b)
