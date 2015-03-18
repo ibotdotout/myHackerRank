ebd66f1b23a490bf63ee0af446b315ea55639dc5
ibotdotout <tkroputa@gmail.com>
2015-03-18 16:09:50 +0700

    clean it not finish yet

--- algoritms/search/number_list.py
+++ algoritms/search/number_list.py
@@ -4,20 +4,13 @@
 def solve(l, k):
     n = len(l)
     result = 0
-    for w in range(n):
-        life_time, m = 0, 0
-        for j in range(w, n):
-            if life_time > 0:
-                if l[j] > m:
-                    life_time = w
-                    m = l[j]
-            else:
-                ll = l[j-w:j+1]
-                m = max(ll)
-                life_time = ll.index(m)
+    for i in range(n):
+        m = l[i]
+        for v in l[i:]:
+            if v > m:
+                m = v
             if m > k:
                 result += 1
-            life_time -= 1
     return result
 
 
