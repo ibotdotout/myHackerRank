7b02462e01e67cd382ecdb0a7b3b57fd705fd920
ibotdotout <tkroputa@gmail.com>
2015-03-24 22:55:56 +0700

    got solution but dont implement yet

--- algoritms/combinatorics/cyclicquadruples.py
+++ algoritms/combinatorics/cyclicquadruples.py
@@ -1,18 +1,36 @@
 # https://www.hackerrank.com/challenges/cyclicquadruples
+# solution
+# http://hr-filepicker.s3.amazonaws.com/infinitum-may14/477-cyclicquadruples.pdf
+# not implement yet
+
+
+def overlap_range(p1, p2):
+    if p1 and p2:
+        p1, p2 = sorted((p1, p2))
+        x, y = p1
+        a, b = p2
+        if a <= y and a >= x:
+            return (a, min(y, b))
+    return None
+
+
+def overlap_set(prev_set, new_set):
+    return [overlap_range(p, n) for p, n in zip(prev_set, new_set)]
+
+
+def range_set(sets):
+    return map(lambda x: (x[1] + 1) - x[0] if x else 0, sets)
 
 
 def solve(seq):
-    mod = 10 ** 9 + 7
-    possible_seq = [[i] for i in range(seq[0], seq[1]+1)]
-    for i in range(1, len(seq)//2):
-        l, r = seq[i*2: (i+1)*2]
-        tmp_seq = []
-        for i in possible_seq:
-            for j in range(l, r+1):
-                if i[-1] != j:
-                    tmp_seq.append(i + [j])
-        possible_seq = tmp_seq
-    return sum(i[3] != i[0] for i in possible_seq) % mod
+    # mod = 10 ** 9 + 7
+    sets = [seq[i*2: (i+1)*2] for i in range(len(seq)//2)]
+
+    overlap = [sets]
+    for i in range(3):
+        new_sets = sets[i+1:] + sets[0:i+1]
+        ov = overlap_set(overlap[-1], new_sets)
+        overlap.append(ov)
 
 
 if __name__ == '__main__':
