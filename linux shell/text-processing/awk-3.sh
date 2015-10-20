0be6f5ba84f6a03598d532e463cd1a88d70ac9f2
ibotdotout <tkroputa@gmail.com>
2015-10-20 21:30:20 +0700

    Add awk

---
+++ linux shell/text-processing/awk-3.sh
@@ -0,0 +1,6 @@
+awk '{ score=($2+$3+$3)/3; \
+if (score >= 80) grade = "A"; \
+else if (score >= 60) grade = "B"; \
+else if(score >= 50) grade= "C"; \
+else grade = "FAIL "; \
+print $0,":",grade }'
