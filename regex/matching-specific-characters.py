bce0d3c5ba0c940530482537594bdcc3240577fe
ibotdotout <tkroputa@gmail.com>
2016-08-20 21:51:43 +0700

    add(regex) - matching-specific-characters.py

---
+++ regex/matching-specific-characters.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/matching-specific-characters
+
+import re
+
+Regex_Pattern = r'^[123][120][xs0][30Aa][xsu][\.,]$'  # Do not delete 'r'.
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
