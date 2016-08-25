178a2ffeab209bbc5b3a5fc9aa2e6eb013dda8af
ibotdotout <tkroputa@gmail.com>
2016-08-26 00:08:11 +0700

    add(regex): matching-one-or-more-repititions.py

---
+++ regex/matching-one-or-more-repititions.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/matching-one-or-more-repititions
+
+import re
+
+Regex_Pattern = r'^\d+[A-Z]+[a-z]+$'
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
