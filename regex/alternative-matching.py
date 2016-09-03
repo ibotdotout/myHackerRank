9a2d54af6fd41bb6c33c54b88f1a8af848d92c5a
ibotdotout <tkroputa@gmail.com>
2016-09-03 09:09:17 +0700

    add(regex): alternative-matching.py

---
+++ regex/alternative-matching.py
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/alternative-matching
+
+import re
+
+Regex_Pattern = r'(^(Mr|Mrs|Ms|Dr|Er)\.[a-zA-Z]+$)'
+
+print(str(bool(re.search(Regex_Pattern, input()))).lower())
