7229f2860bc015a568d015317cff5bb6b448746e
botbotbot <tkroputa@gmail.com>
2016-09-18 17:02:11 +0700

    add(fp): area-of-a-polygon.clj

---
+++ fp-clojure/area-of-a-polygon.clj
@@ -0,0 +1,13 @@
+;; https://www.hackerrank.com/challenges/lambda-march-compute-the-area-of-a-polygon
+;; http://www.mathopenref.com/coordpolygonarea.html
+
+(defn calCross [[x1 y1] [x2 y2]]
+  (- (* x1 y2) (* y1 x2))
+)
+
+(let [n (read)
+      points (cycle (repeatedly n #(vec [(read) (read)])))
+      lst (take n points)
+      rst (rest points)]
+     (println (/ (Math/abs (reduce + (map calCross lst rst))) 2))
+)
