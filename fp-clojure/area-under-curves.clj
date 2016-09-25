e06752bbd0baef0c627d22b8c3d0db11e2d67510
botbotbot <tkroputa@gmail.com>
2016-09-25 15:26:08 +0700

    update(fp): area-under-curves.clj

--- fp-clojure/area-under-curves.clj
+++ fp-clojure/area-under-curves.clj
@@ -1,32 +1,15 @@
-; https://www.hackerrank.com/challenges/area-under-curves-and-volume-of-revolving-a-curv/
-
 ; summation of f(x) * delta(x) with value of x ranging from lower limit to upper limit by delta(x) steps
 ; http://tutorial.math.lamar.edu/Classes/CalcI/Area_Volume_Formulas.aspx
 
-(require '[clojure.string :as str])
-
-(defn listInt []
-  (map #(Float/parseFloat %) (str/split (read-line) #" "))
-)
-
-(defn fx [a b x]
-    (reduce + (map #(* %1 (Math/pow x %2)) a b))
-) 
-
-(defn area [a b steps deltaX]
-   (* (reduce + (map #(fx a b %) steps)) deltaX)
-)
-
-(defn volume [a b steps deltaX]
-   (* (reduce + (map #(Math/pow (fx a b %) 2) steps)) deltaX 3.14)
-)
-
-
-(let [a (listInt)
-      b (listInt)
-      [L R] (listInt)
-      steps (range L R 0.001)
-      deltaX (/ (- R L) (count steps))]
-      (println (area a b steps deltaX))
-      (println (volume a b steps deltaX))
+(let [read-nums (fn [] (map #(Float/parseFloat %) (clojure.string/split (read-line) #"\s+")))      
+      coeffs (read-nums)
+      exponents (read-nums)
+      [low high] (read-nums)
+      dx 0.001
+      fx (fn [x] (->> exponents (map #(Math/pow x %)) (map * coeffs) (reduce +)))
+      area (fn [r] (->> r (map fx) (reduce +) (* dx)))
+      volume (fn [r] (->> r (map fx) (map #(* dx % % Math/PI)) (reduce +)))
+      steps (range low (+ high dx) dx)]
+      (println (area steps))
+      (println (volume steps))
 )
