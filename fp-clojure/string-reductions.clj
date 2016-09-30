ca4af0a148aae27cb5f40b9a38ec45d930cca01d
botbotbot <tkroputa@gmail.com>
2016-09-30 12:51:43 +0700

    update(fp): string-reductions.clj

--- fp-clojure/string-reductions.clj
+++ fp-clojure/string-reductions.clj
@@ -3,4 +3,13 @@
 ; about contains? 
 ; http://stackoverflow.com/a/11963279
 
-(println (clojure.string/join (reduce #(if (contains? (set %1) %2) %1 (conj %1 %2)) [] (read-line))))
+(->>
+  (read-line)
+  (reduce #(if (contains? (set %1) %2) %1 (conj %1 %2)) [] )
+  (apply str)
+  println
+)
+
+; shortest answer
+; (println (apply str (distinct (read-line))))
+
