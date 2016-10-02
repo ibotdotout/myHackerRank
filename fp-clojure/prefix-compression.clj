93b834d664035b7db614cefc896d5737684066ac
botbotbot <tkroputa@gmail.com>
2016-10-02 23:08:08 +0700

    update(fp): prefix-compression.clj

--- fp-clojure/prefix-compression.clj
+++ fp-clojure/prefix-compression.clj
@@ -1,5 +1,7 @@
 ; https://www.hackerrank.com/challenges/prefix-compression
 
+; first
+
 (let [a (read-line)
       b (read-line) 
       parts (->> (map #(vec [%1 %2]) a b) (partition-by #(= (first %) (second %))))
@@ -9,3 +11,12 @@
         (println (- (count a) n-prefix) (apply str (drop n-prefix a)))
         (println (- (count b) n-prefix) (apply str (drop n-prefix b)))
       ))
+
+; better
+
+(let [a (read-line)
+      b (read-line) 
+      n-prefix (->> (map = a b) (take-while identity) (count))
+      answers (#(vec [(subs a 0 %) (subs a %) (subs b %)]) n-prefix)]
+     (doseq [i answers] 
+      (println (#(str (count %) " " %) i))))
