370455914154f2a67c46649f0b0e17573a02dc4c
botbotbot <tkroputa@gmail.com>
2016-11-08 00:30:48 +0700

    Update sequence-full-of-colors.clj

--- fp-clojure/sequence-full-of-colors.clj
+++ fp-clojure/sequence-full-of-colors.clj
@@ -55,3 +55,28 @@
     (let [word (read-line)
           prefixs (getPrefixs word)]
          (printAnswer (and (checkFullConditions word) (checkPrefixConditions prefixs))))))
+
+; pass
+(defn abs [x] (max x (- x))) ; this better than (Math/abs) that make timeout
+(defn eq [stats a b] (= (stats a) (stats b)))
+(defn diff-one [stats a b] (-> (- (stats a) (stats b)) (abs) (<= 1)))
+
+(defn anwser [[result stats]]
+  (if (and result (eq stats \R \G) (eq stats \Y \B))
+     "True"
+     "False"))
+
+(defn sequence-full-of-colors [word]
+  (->> word
+    (reduce (fn [[result stats] code]
+      (if (and (diff-one stats \R \G) (diff-one stats \Y \B))
+        [result (assoc stats code (inc (stats code)))]
+        (reduced [false]))) 
+      [true {\R 0 \G 0 \Y 0 \B 0}])
+    (anwser)
+    (println)))
+    
+(let [t (Integer/parseInt (read-line))]
+  (dotimes [_ t]
+    (let [word (read-line)]
+      (sequence-full-of-colors word))))
